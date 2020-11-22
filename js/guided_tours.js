9481c4d0250200f10822f67afaba511905d3ba36
christianfelt <christianfelt@comcast.net>
2020-11-22 12:12:39 -0700

    almonds storytelling drafted

--- js/guided_tours.js
+++ js/guided_tours.js
@@ -3,16 +3,40 @@ class GuidedTours {
         this.data = data;
         this.cropVis = cropVis;
         this.cropVis.guidedTours = this;
+        this.almondsString = "In 1961, Spain produced more than twice as many almonds as any other country. Spain's production did not change much overall between 1961 and 2014, but during the same period, almond production in the United States grew dramatically. Around 80% of the world's almonds are now produced in the state of California, and about 70% of California's crop is exported overseas.";
+    }
+    deleteOverlay() {
+        d3.selectAll(".overlay").remove();
     }
     drawGuidedTours() {
         let that = this;
-        d3.select("#almondsImg").on("click", d => that.almondsTour(that));
+        d3.select("#almondsImg").on("click", function () { that.almondsTour(that) });
     }
-    almondsTour(that){
-        d3.select("body").append("div").attr("id", "almondsOverlay").classed("overlay", true);
+    almondsTour(that) {
+        let overlay = d3.select("#guided_tours_wrapper")
+            .append("div")
+            .attr("id", "almondsOverlay")
+            .classed("overlay", true);
+        overlay.append("p")
+            .classed("overlayText", true)
+            .text(that.almondsString);
+        overlay.append("button")
+            .attr("type", "button")
+            .on("click", function () {
+                that.deleteOverlay();
+            })
+            .text("Back");
         let thisLi = document.getElementById("Almonds_with_shell");
-        console.log(thisLi);
         that.cropVis.cropList.updateCropSelection("Almonds, with shell", that.cropVis.cropList, thisLi);
+        that.cropVis.worldMap.clearHighlightedBoundaries();
+        that.cropVis.barChart.deleteBarChart();
+        that.cropVis.lineChart.deleteLineChart();
+        that.cropVis.selected_countries.clear();
+        for (let country of ["USA", "Spain", "Australia", "Iran", "Morocco"]) {
+            that.cropVis.selected_countries.add(country);
+            that.cropVis.barChart.updateBarChart();
+        }
+        that.cropVis.lineChart.updateLineChart();
     }
     selectPreset(crop, countries, year) {
 
